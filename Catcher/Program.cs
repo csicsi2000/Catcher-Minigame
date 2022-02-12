de317b85c2f465b10a584e3868f565813d160a6c
csicsi2000 <69510072+csicsi2000@users.noreply.github.com>
2022-02-12 09:32:40 +0100

    Updated

--- Catcher/Program.cs
+++ Catcher/Program.cs
@@ -1,145 +1,192 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
 
 namespace Catcher
 {
     class Program
     {
-        static int yPosition ;
-        static int xPosition ;
+        static int yPosition;
+        static int xPosition;
 
         static int chaserY;
         static int chaserX;
 
         static int difficulty;
+        static char diff;
         static int score;
 
         static bool gameEnd;
+        static bool restart = true;
+
+        static int[] speeds = { 1000, 750, 500, 250, 150, 100 };
         static void Main(string[] args)
         {
-        start:
-            xPosition = 0;
-            yPosition = 0;
-            chaserX = 9;
-            chaserY = 9;
-            score = 0;
-            gameEnd = false;
+            while (restart)
+            {
+                xPosition = 0;
+                yPosition = 0;
+                chaserX = 9;
+                chaserY = 9;
+                score = 0;
+                gameEnd = false;
 
-            Console.WriteLine("Escape the chaser! ");
-            Console.WriteLine("Your character is X");
-            Console.WriteLine("Enemy character is ¤");
-            Console.WriteLine("Use W,A,S,D to move your character");
-            Console.Write("Choose you difficulty with number (1 is the easiest, 6 is the hardest): ");
+                Console.WriteLine("Run form the chaser! ");
+                Console.WriteLine("X is your character");
+                Console.WriteLine("The enemy is ¤");
+                Console.WriteLine("Use W,A,S,D to move");
+                Console.Write("Choose difficulty (1 is the easiest, 6 is the hardest): ");
+
+                SetDifficulty();
+
+                CountDown();
+
+                StartGame();
+
+                PrintBoard();
+                Console.WriteLine();
+                Console.WriteLine("He catched you!");
+                Console.WriteLine("\nYour Score: " + score * Convert.ToInt32(diff));
+                Console.WriteLine();
+
+                restart = IsPlayAgain();
+            }
+        }
+        static void CountDown()
+        {
+            Console.WriteLine();
+            Console.Write("The game begins in ");
+            Console.WriteLine();
+            for (int i = 3; i > 0; i--)
+            {
+                Thread.Sleep(900);
+                Console.Write(i + " ");
+            }
+            Thread.Sleep(900);
+        }
+        static bool IsPlayAgain()
+        {
+            Console.WriteLine("Try again? y/n");
+
+            while (true)
+            {
+                char again = Console.ReadKey().KeyChar;
+                if (again == 'y')
+                {
+                    Console.Clear();
+                    return true;
+                }
+                else if (again == 'n')
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Non valid character");
+                }
+            }
+        }
+
+        static void StartGame()
+        {
+            Console.Clear();
+            Console.CursorVisible = false;
+
+            var backgroundThread = new Thread(LoopMove);
+            backgroundThread.Start();
+
+            int count = 0;
+
+            while (!gameEnd)
+            {
+                char key = Console.ReadKey().KeyChar;
+                bool valid = Move(key);
+
+                if (!valid)
+                {
+                    Console.WriteLine("Non valid movement");
+                }
+
+                if(count == 20)
+                {
+                    difficulty = difficulty - 50;
+                    count = 0;
+                }
+                count++;
+            }
+            backgroundThread.Abort();
+
+            Console.Clear();
+            Console.CursorVisible = true;
+        }
+
+        static void SetDifficulty()
+        {
             bool IsValid = false;
+
+
             while (!IsValid)
             {
-                char diff = Console.ReadKey().KeyChar;
+                diff = Console.ReadKey().KeyChar;
                 if (diff == '1')
                 {
-                    difficulty = 1000;
+                    difficulty = speeds[0];
                     IsValid = true;
                 }
                 else if (diff == '2')
                 {
-                    difficulty = 750;
+                    difficulty = speeds[1];
                     IsValid = true;
 
                 }
                 else if (diff == '3')
                 {
-                    difficulty = 500;
+                    difficulty = speeds[2];
                     IsValid = true;
 
                 }
                 else if (diff == '4')
                 {
-                    difficulty = 250;
+                    difficulty = speeds[3];
                     IsValid = true;
 
                 }
                 else if (diff == '5')
                 {
-                    difficulty = 150;
+                    difficulty = speeds[4];
                     IsValid = true;
 
                 }
                 else if (diff == '6')
                 {
-                    difficulty = 100;
+                    difficulty = speeds[5];
                     IsValid = true;
 
                 }
                 else
                 {
                     Console.WriteLine();
-                    Console.WriteLine("Not valid difficulty setting");
+                    Console.WriteLine("Non valid number for difficulty");
                     IsValid = false;
                 }
             }
-            Console.WriteLine();
-            Console.Write("Game starting in ");
-            Console.WriteLine();
-            for(int i = 3; i > 0; i--)
-            {
-                Thread.Sleep(900);
-                Console.Write( i +" ");
-            }
-            var background = new ThreadStart(LoopMove);
-            var backgroundThread = new Thread(background);
-            backgroundThread.Start();
-
-            var boardT = new ThreadStart(Board);
-            var boardLoop = new Thread(boardT);
-            boardLoop.Start();
-
-            while (!gameEnd)
-            {
-                char key =Console.ReadKey().KeyChar;
-                bool valid = Move(key);
-
-                if (!valid)
-                {
-                    Console.WriteLine("Invalid movement");
-                }
-            }
-            backgroundThread.Abort();
-            boardLoop.Abort();
-
-            Console.WriteLine();
-            Console.WriteLine("He catched you!");
-            Console.WriteLine("Your score is: " + score);
-            Console.WriteLine();
-            Console.WriteLine("Try again? y/n");
-
-            bool YN = false;
-            while (!YN)
-            {
-                char again = Console.ReadKey().KeyChar;
-                if (again == 'y') 
-                {
-                    Console.Clear();
-                    goto start;
-                }
-                else if(again == 'n')
-                {
-                    YN = true;
-                }
-                else
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Invalid character");
-                }
-            }
         }
-
-        static bool Move (char direction)
+        static bool Move(char direction)
         {
+            Console.SetCursorPosition(0, 10);
+            Console.Write("                             ");
+            Console.SetCursorPosition(0, 10);
+            Console.SetCursorPosition(0, 11);
+            Console.Write("  ");
+
+
+            if (gameEnd)
+            {
+                return true;
+            }
             bool success = false;
-            if(direction == 'w' || direction == 'W')
+            if (direction == 'w' || direction == 'W')
             {
                 if (Check(yPosition - 1))
                 {
@@ -171,7 +218,10 @@ namespace Catcher
                     success = true;
                 }
             }
-            
+
+            PrintBoard();
+
+            IsGameEnd();
 
             return success;
         }
@@ -187,51 +237,56 @@ namespace Catcher
             }
         }
 
-        public static void Board()
+        public static void PrintBoard()
         {
-            while (!gameEnd)
+            Console.SetCursorPosition(0, 0);
+
+            StringBuilder printBoard = new StringBuilder();
+            for (int i = 0; i < 10; i++)
             {
-                Thread.Sleep(difficulty);
-                Console.WriteLine();
-                Console.WriteLine();
-                StringBuilder printBoard = new StringBuilder();
-                for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
                 {
-                    for (int j = 0; j < 10; j++)
+                    if (i == chaserY && j == chaserX)
                     {
-                        if (i == yPosition && j == xPosition)
-                        {
-                            printBoard.Append("X ");
-                        }
-                        else if (i == chaserY && j == chaserX)
-                        {
-                            printBoard.Append("¤ ");
-                        }
-                        else
-                        {
-                            printBoard.Append(". ");
-                        }
+                        printBoard.Append("¤ ");
                     }
-                    printBoard.Append("\n");
+                    else if (i == yPosition && j == xPosition)
+                    {
+                        printBoard.Append("X ");
+                    }
+                    else
+                    {
+                        printBoard.Append(". ");
+                    }
                 }
-                Console.WriteLine(printBoard);
+                printBoard.Append("\n");
             }
+
+            Console.Write(printBoard);
         }
 
         public static void LoopMove()
         {
             while (!gameEnd)
             {
-                if (chaserX == xPosition && chaserY == yPosition)
-                {
-                    gameEnd = true;
-                }
+                IsGameEnd();
+
                 Thread.Sleep(difficulty);
                 Chase();
 
+                PrintBoard();
+
                 score++;
             }
-        }     
+        }
+
+        public static void IsGameEnd()
+        {
+            if (chaserX == xPosition && chaserY == yPosition)
+            {
+                gameEnd = true;
+            }
+        }
 
         public static void Chase()
         {
@@ -241,16 +296,16 @@ namespace Catcher
             if (xDiff > yDiff)
             {
                 int chaseD = chaserX - xPosition;
-                if(chaseD > 0)
+                if (chaseD > 0)
                 {
                     chaserX = chaserX - 1;
                 }
-                if(chaseD < 0)
+                if (chaseD < 0)
                 {
                     chaserX = chaserX + 1;
                 }
             }
-            else if(yDiff >= xDiff)
+            else if (yDiff >= xDiff)
             {
                 int chaseD = chaserY - yPosition;
                 if (chaseD > 0)
@@ -265,3 +320,4 @@ namespace Catcher
         }
     }
 }
+
